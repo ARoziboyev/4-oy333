36d27647046437155a40f37fe477971c0f84f105
ARoziboyev <arozibpyev212@gmail.com>
2024-12-02 13:59:19 +0500

    первый коммит

---
+++ index.js
@@ -0,0 +1,379 @@
+// 1.Berilgan ikkita sonning har biri musbat ekanligini aniqlang.
+
+//  let num1 = +prompt("sonni kiriting")
+//  let num2 = +prompt("sonni kiriting")
+
+
+//  if (num1 > 0 && num2 > 0) {
+//     console.log("ikkalasiham musbat");
+//  }else{
+//     console.log("musbat emas");
+//  }
+
+// 2.Uchta son berilgan. Ulardan hech bo'lmaganda bittasi manfiy bo'lishi mumkinmi?
+
+
+// let num1 = +prompt("1-sonni kiriting")
+// let num2 = +prompt("2-sonni kiriting")
+// let num3 = +prompt("3-sonni kiriting")
+
+
+// if (num1 < 0 || num2 < 0 || num3 < 0) {
+//     console.log("manfi");
+// }else{
+//     console.log("musbat son kiritdingiz");
+// }
+
+//3. Berilgan son juft va musbatmi?
+
+
+
+// 4.Ikki sonning yig'indisi ularning ko'paytmasidan katta ekanligini tekshiring.
+
+// let num1 = +prompt("1-sonni kiriting")
+// let num2 = +prompt("2-sonni kiriting")
+
+
+// if (num1 + num2 > num1 * num2 ){
+//     console.log("javob to'g'ri");
+// }else{
+//     console.log("javob noto'gri");
+// }
+
+// 5.Berilgan son 100 dan katta va 999 dan kichik ekanligini aniqlang.
+
+// let num = +prompt("sonni kiriting")
+
+// if (num > 100 && num < 999) {
+//     console.log("javob to'g'ri");
+// }else{
+//     console.log("javob noto'g'ri");
+    
+// }
+
+// 6.Uchta sonning yig'indisi musbat ekanligini tekshiring.
+
+// let num1 = +prompt("1sonni kiriting")
+// let num2 = +prompt("2-sonni kiriting")
+// let num3 = +prompt("3-sonni kiriting")
+
+// if (num1 + num2 + num3 >0) {
+//     console.log("javob to'g'ri");
+// }else{
+//     console.log("javob noto'g'ri");
+    
+// }
+
+
+// 7.Berilgan ikkita sonning biri 0 bo'lmasligini tekshiring.
+
+// let num1 = +prompt("1-sonni kiriting")
+// let num2 = +prompt("2-sonni kiriting")
+
+// if (num1 !== 0 && num2 !== 0) {
+//     console.log("javob to'g'ri");
+// }else{
+//     console.log("javob noto'g'ri");
+    
+// }
+
+
+// 8.Ikki sonning ko'paytmasi musbat va ularning ayirmasi manfiy ekanligini aniqlang.
+
+// let num1 = +prompt("1-sonni kiriting")
+// let num2 = +prompt("2-sonni kiriting")
+
+
+// if (num1 * num2 > 0  || num1 - num2 > 0) {
+//     console.log("javob to'g'ri");
+// }else{
+//     console.log("javob noto'g'ri");
+    
+// }
+
+
+// 9.Berilgan son ikki xonali musbat sonmi?
+
+// let num = +prompt("sonni kiriting")
+
+// if (num > 0) {
+//     console.log("javob to'g'ri");
+// }else{
+//     console.log("javob noto'g'ri");
+    
+// }
+
+
+// 10.Berilgan son juft va 5 ga karrali ekanligini tekshiring
+
+// let num = +prompt("sonni kiriting")
+
+// if (num % 5 == 0) {
+//     console.log("javob to'g'ri");
+// }else{
+//     console.log("javob noto'g'ri");
+    
+// }
+
+
+// 11.Uchta sonning hech bo'lmaganda ikkitasi teng ekanligini aniqlang
+
+// let num1 = +prompt("1-sonni kiriting")
+// let num2 = +prompt("2-sonni kiriting")
+// let num3 = +prompt("3-sonni kiriting")
+
+// if (num1 === num2 || num2 === num3 || num1 === num3) {
+//     console.log("javob to'g'ri");
+// }else{
+//     console.log("javob noto'g'ri");
+    
+// }
+
+// 12.Berilgan son 7 ga bo'linadi va 3 ga bo'linmaydimi?
+
+// let num = +prompt("sonni kiriting")
+
+//     if (num % 7 == 0 && num % 3 != 0) {
+//       console.log(false);
+//     }else{
+//         console.log(true);
+//     }
+
+
+// 13.Ikkita sonning biri 0 yoki ikkalasi ham manfiy ekanligini tekshiring.
+
+
+// let num = +prompt("sonni kiriting")
+
+// if (num > 0) {
+//     console.log("musbat son");
+// }else{
+//     console.log("manfiy");
+// }
+
+
+// 14.Uchta sonning eng kattasi musbat ekanligini aniqlang.
+
+// let num1 = +prompt("1-sonni kiriting")
+// let num2 = +prompt("2-sonni kiriting")
+// let num3 = +prompt("3-sonni kiriting")
+
+
+// if (num1 > num2 || num2 > num3 && num1 > 0 && num2 > 0 && num3 > 0) {
+
+//     console.log("kattasi musbat");
+// }else{
+//     console.log("kichigi musbat");
+// }
+
+// 15.Berilgan son 10 va 20 orasida emasligini aniqlang.
+// let num = +prompt("sonni kiriting")
+// if (num < 10 && num > 20) {
+//     console.log("10 va 20ni orasida kiritgan raqamingiz");
+// }else{
+//     console.log(" kiritgan raqamingiz  10 va 20ni orasida emas");
+// }
+
+
+// 16.Ikkita sonning yig'indisi manfiy yoki ularning ko'paytmasi musbat ekanligini tekshiring.
+
+//  bajarolmadim
+
+
+// 17.Uchta sonning eng kichigi juft ekanligini aniqlang.
+
+
+// let num1 = +prompt("sonni kiriting")
+// let num2 = +prompt("sonni kiriting")
+// let num3 = +prompt("sonni kiriting")
+
+// if (num1 % 2 == 0  && num2 % 2 == 0  && num2 % 2 == 0) {
+//     console.log("juft");
+// }else{
+//     console.log("juft emas");
+// }
+
+// 18.Berilgan sonning oxirgi raqami 5 yoki 0 ekanligini tekshiring.
+
+
+// let num = +prompt("sonni kiriting")
+
+// if (num % 10 == 5) {
+//     console.log(true);
+// }else{
+//     console.log(false);
+// }
+
+// 19.Ikki sonning biri musbat va ikkinchisi manfiy ekanligini aniqlang.
+
+// let num1 = +prompt("sonni kiriting")
+// let num2 = +prompt("sonni kiriting")
+
+// if (num1 > 0 && num2 > 0) {
+//     console.log(true);
+// }else{
+//     console.log(false);
+    
+// }
+
+
+// 22.Berilgan ikki sonning biri ikkinchisiga karrali ekanligini tekshiring.
+
+//  let num1  = +prompt("sonni kiriting")
+//  let num2  = +prompt("sonni kiriting")
+
+// if (num1 % num2 == 0) {
+//     console.log(true);
+// }else{
+//     console.log(false);
+// }
+
+// 23.Uchta sonning yig'indisi 0 yoki ko'paytmasi manfiy ekanligini aniqlang.
+
+// bajarolmadim
+
+
+// 25.Ikki sonning biri toq va ikkinchisi juft ekanligini aniqlang.
+
+// let num1 = +prompt("sonni kiriting")
+// let num2 = +prompt("sonni kiriting")
+
+// if (num1 % 2 !== 0 && num2 % 2 !== 0) {
+//     console.log(false);
+// }else{
+//     console.log(true);
+// }
+
+// 26.Berilgan son musbat va uch xonali emasligini aniqlang.
+
+// let num = +prompt("sonni kiriting")
+
+// if (num > 0 && num < 100) {
+//     console.log(true);
+// }else{
+//     console.log(false);
+// }
+
+// 27.Uchta sonning yig'indisi 100 dan katta yoki ko'paytmasi manfiy ekanligini tekshiring.
+
+// let num1 = +prompt("sonni kiriting")
+// let num2 = +prompt("sonni kiriting")
+// let num3 = +prompt("sonni kiriting")
+
+// if (num1 + num2 + num3 > 100) {
+//     console.log(true);
+// }else{
+//     console.log(false);
+// }
+
+// 28.ikki sonning biri 10 dan katta va ikkinchisi 0 dan kichik ekanligini aniqlang.
+
+// let num1 = +prompt("sonni kiriting")
+// let num2 = +prompt("sonni kiriting")
+
+// if (num1 >10 && num2 < 0 || num2>10 && num1<0) {
+//     console.log(true);
+// }else{
+//     console.log(false);
+// }7
+
+
+// 29.Berilgan son toq va musbatmi?
+
+// let num = +prompt("sonni kiriting")
+
+// if (num > 0 && num % 2 !== 0) {
+//     console.log(true);
+// }else{
+//     console.log(false);
+// }
+
+
+// 30.Ikki sonning hech bo'lmaganda bittasi musbat ekanligini tekshiring
+
+// let num1 = +prompt("sonni kiriting")
+// let num2 = +prompt("sonni kiriting")
+
+// if (num1 > 0 && num2 > 0 ) {
+//     console.log(true);
+// }else{
+//     console.log(false);
+    
+// }
+
+
+//31. Berilgan son juft va 4 ga karrali, lekin 8 ga karrali emasligini aniqlang.
+
+
+// let nun1=+prompt("son")
+
+// if (num1%2==0 && num1%4==0 && num1%8!=0) {
+//     console.log(true);
+    
+// }else{console.log(false);
+// }
+
+// 32.Uchta sonning faqat bittasi manfiy ekanligini tekshiring.
+
+// let num1 = +prompt("sonni kiriting")
+// let num2 = +prompt("sonni kiriting")
+// let num3 = +prompt("sonni kiriting")
+
+
+// if (num1 < 0 && num2 < 0 && num3 < 0) {
+//     console.log(true);
+// }else{
+//     console.log(false);
+    
+// }
+
+
+// 34.Ikkita sonning yig'indisi ularning farqidan katta ekanligini aniqlang.
+
+// let num1 = +prompt("sonni kiriting")
+// let num2 = +prompt("sonni kiriting")
+
+// if (num1 + num2 || num1 > num2) {
+//     console.log(true);
+// }else{
+//     console.log(false);
+    
+// }
+
+
+// 35.Berilgan son 0 dan katta va 1000 dan kichik ekanligini tekshiring.
+
+// let num = +prompt("sonni kiriting")
+
+// if (num > 0 && num  < 1000) {
+//     console.log(true);
+// }else{
+//     console.log(false);
+    
+// }
+
+// 36.Ikki sonning biri juft va ularning ko'paytmasi 20 dan katta ekanligini aniqlang
+// bajarolmadim
+
+// 37.Uchta sonning hech bo'lmaganda bittasi 10 ga bo'linadi ekanligini tekshiring.
+
+// let num1 = +prompt("sonni kiriting")
+// let num2 = +prompt("sonni kiriting")
+// let num3 = +prompt("sonni kiriting")
+
+// if (num1 > 10 && num2 > 10 && num3 > 10) {
+//     console.log(true);
+    
+// }
+
+// 38.Berilgan son 4 ga karrali va toq emasligini aniqlang
+
+// let num = +prompt("sonni kiriting")
+
+// if (num % 4 == 0 || num % 2 !== 0) {
+//     console.log(true);
+    
+// }
+
+// 39.Uchta sonning ikkitasining yig'indisi uchinchisidan katta ekanligini tekshiring.
+// bajarolmadim
